0589e8fe5b1a00569c30195cf5efc34d70733144
loic <loic.lequellec@edu.ece.fr>
2022-11-02 15:20:36 +0100

    raylib

---
+++ structure et macros/STRUCTURE.h
@@ -0,0 +1,29 @@
+//
+// Created by leque on 01/11/2022.
+//
+
+#ifndef ECE_CITY_3H_STRUCTURE_H
+#define ECE_CITY_3H_STRUCTURE_H
+#include "raylib.h"
+struct element{
+    Color color;///remplacer par image plus tard
+    int nb_habitants;// 0 si pas habitant
+    int prix;// -1 si pas achetable
+    int espacement_x;
+    int espacement_y;
+}element;
+
+struct joueur {
+    int element_choisie;
+}joueur;
+struct ece_city{///ajouter compteur temps
+    int plateau[colones][ligne];//45 colonnes sur 35 lignes
+    struct element tableau_element[10];
+    struct joueur joueur1;
+    int etage;
+    int ece_flouz;
+    int nb_habitant;
+    int nb_electricite;
+    int nb_eau;
+}city;
+#endif //ECE_CITY_3H_STRUCTURE_H
---
+++ structure et macros/macros.h
@@ -0,0 +1,12 @@
+//
+// Created by leque on 01/11/2022.
+//
+
+#ifndef ECE_CITY_3H_MACROS_H
+#define ECE_CITY_3H_MACROS_H
+
+#define ligne 34// egal nombre de colonnes -1
+#define colones 44// egal nombre de ligne -1
+#define espacement 15
+
+#endif //ECE_CITY_3H_MACROS_H
